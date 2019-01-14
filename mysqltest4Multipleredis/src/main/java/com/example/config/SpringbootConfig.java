1de75169caba211e59e9adcfb2c357fbb16c66cc
hzliubenlong <hzliubenlong@corp.netease.com>
2019-01-14 14:25:31 +0800

    多redis数据源

--- mysqltest4Multipleredis/src/main/java/com/example/config/SpringbootConfig.java
+++ mysqltest4Multipleredis/src/main/java/com/example/config/SpringbootConfig.java
@@ -20,53 +20,86 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Configuration
 public class SpringbootConfig {
 
+    /**
+     * 配置lettuce连接池
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.redis.lettuce.pool")
-    @Scope(value = "prototype")
     public GenericObjectPoolConfig redisPool() {
         return new GenericObjectPoolConfig<>();
     }
 
+    /**
+     * 配置第一个数据源的
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.redis")
     public RedisStandaloneConfiguration redisConfig() {
         return new RedisStandaloneConfiguration();
     }
 
+    /**
+     * 配置第二个数据源
+     * @return
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.redis2")
     public RedisStandaloneConfiguration redisConfig2() {
         return new RedisStandaloneConfiguration();
     }
 
+    /**
+     * 配置第一个数据源的连接工厂
+     * 这里注意：需要添加@Primary 指定bean的名称，目的是为了创建两个不同名称的LettuceConnectionFactory
+     * @param config
+     * @param redisConfig
+     * @return
+     */
     @Bean("factory")
     @Primary
     public LettuceConnectionFactory factory(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfig) {
-        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder()
-                .poolConfig(config).build();
+        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder().poolConfig(config).build();
         return new LettuceConnectionFactory(redisConfig, clientConfiguration);
     }
 
     @Bean("factory2")
     public LettuceConnectionFactory factory2(GenericObjectPoolConfig config, RedisStandaloneConfiguration redisConfig2) {
-        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder()
-                .poolConfig(config).build();
+        LettuceClientConfiguration clientConfiguration = LettucePoolingClientConfiguration.builder().poolConfig(config).build();
         return new LettuceConnectionFactory(redisConfig2, clientConfiguration);
     }
 
+    /**
+     * 配置第一个数据源的RedisTemplate
+     * 注意：这里指定使用名称=factory 的 RedisConnectionFactory
+     * 并且标识第一个数据源是默认数据源 @Primary
+     * @param factory
+     * @return
+     */
     @Bean("redisTemplate")
     @Primary
     public RedisTemplate<String, String> redisTemplate(@Qualifier("factory")RedisConnectionFactory factory) {
-        StringRedisTemplate template = new StringRedisTemplate(factory);
-        template.setKeySerializer(RedisSerializer.string());
-        template.setValueSerializer(new FastJsonRedisSerializer<>(Object.class));
-        template.setHashKeySerializer(RedisSerializer.string());
-        template.setHashValueSerializer(new FastJsonRedisSerializer<>(Object.class));
-        return template;
+        return getStringStringRedisTemplate(factory);
     }
 
+    /**
+     * 配置第一个数据源的RedisTemplate
+     * 注意：这里指定使用名称=factory2 的 RedisConnectionFactory
+     * @param factory2
+     * @return
+     */
     @Bean("redisTemplate2")
     public RedisTemplate<String, String> redisTemplate2(@Qualifier("factory2") RedisConnectionFactory factory2) {
+        return getStringStringRedisTemplate(factory2);
+    }
+
+    /**
+     * 设置序列化方式 （这一步不是必须的）
+     * @param factory2
+     * @return
+     */
+    private RedisTemplate<String, String> getStringStringRedisTemplate(RedisConnectionFactory factory2) {
         StringRedisTemplate template = new StringRedisTemplate(factory2);
         template.setKeySerializer(RedisSerializer.string());
         template.setValueSerializer(new FastJsonRedisSerializer<>(Object.class));
