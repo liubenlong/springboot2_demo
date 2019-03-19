b5c0bdfa463a48880f570a27ef91eb278b2f5720
hzliubenlong <hzliubenlong@corp.netease.com>
2019-03-19 14:44:51 +0800

    freemarker

--- springboot-freemarker/src/main/java/com/example/comtroller/StuController.java
+++ springboot-freemarker/src/main/java/com/example/comtroller/StuController.java
@@ -8,12 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 //Tips: 由于要返回模板页面文件，所以我们只能使用@Controller 而不可以使用@RestController
@@ -32,20 +29,20 @@ public class StuController {
 
 
     @RequestMapping(value = "/welcome")
-    public String hello1(Model m){
+    public String hello1(Model m) {
         m.addAttribute("name", "spring-boot11");
         return "welcome";
     }
 
     @RequestMapping("hello")
-    public ModelAndView hello(ModelAndView m){
+    public ModelAndView hello(ModelAndView m) {
         m.addObject("name", "spring-boot");
         m.setViewName("welcome");
         return m;
     }
 
     @RequestMapping("sysUser")
-    public String user(Model m){
+    public String user(Model m) {
         List<SysUser> list = new ArrayList<>();
         SysUser u1 = new SysUser(0001, "hello1", "11111111111111111");
         SysUser u2 = new SysUser(0002, "hello2", "22222222222222222");
@@ -60,14 +57,31 @@ public class StuController {
 
     @RequestMapping("test1")
     @ResponseBody
-    public String test1(){
+    public String test1() {
         HashMap<Object, Object> map = new HashMap<>();
-        map.put("name","张三");
-        map.put("age",20);
+        map.put("name", "张三");
+        map.put("age", 20);
         String s = freemarkerService.process("test1.ftl", map);
         log.info(s);
         return s;
     }
 
+
+    @RequestMapping("test2")
+    @ResponseBody
+    public String test2() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "FreeMarker是一款用java语言编写的模版引擎");
+        map.put("dateTime", new Date());
+
+        List<SysUser> users = new ArrayList<>();
+        users.add(new SysUser(1, "ITDragon 博客", "111"));
+        users.add(new SysUser(2, "欢迎", "13333333333"));
+        users.add(new SysUser(3, "You！", null));
+        map.put("users", users);
+        String s = freemarkerService.process("test2.ftl", map);
+        log.info(s);
+        return s;
+    }
 
 }
