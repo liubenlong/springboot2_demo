b2e73bc6400557c5a461439bba019ad5cd96ffa1
zhenhun <zhenhun@wacai.com>
2019-12-05 17:55:30 +0800

    LRU

---
+++ leecode-algorithm/src/test/java/LRUCache1.java
@@ -0,0 +1,105 @@
+
+
+import java.util.HashMap;
+
+/**
+ * 方式二：使用hashmap加链表的数据结构
+ *
+ * 缓存采用和Redis一致的Key-Value格式，所以很容易想到hashmap来存储数据。hashmap也可以保证很高的时间复杂度。
+ * 但是hashmap不能存储插入及访问顺序。顺序存储很容易想到链表，所以这里就用链表来记录顺序，读写操作后都调整链表中的节点顺序。
+ *
+ */
+public class LRUCache1<K, V> {
+
+    private final int MAX_CACHE_SIZE;
+    private Entry first;
+    private Entry last;
+
+    private HashMap<K, Entry<K, V>> hashMap;
+
+    public LRUCache1(int cacheSize) {
+        MAX_CACHE_SIZE = cacheSize;
+        hashMap = new HashMap<>();
+    }
+
+    public void put(K key, V value) {
+        Entry entry = getEntry(key);
+        if (entry == null) {
+            if (hashMap.size() >= MAX_CACHE_SIZE) {
+                hashMap.remove(last.key);
+                removeLast();
+            }
+            entry = new Entry();
+            entry.key = key;
+        }
+        entry.value = value;
+        moveToFirst(entry);
+        hashMap.put(key, entry);
+    }
+
+    public V get(K key) {
+        Entry<K, V> entry = getEntry(key);
+        if (entry == null) return null;
+        moveToFirst(entry);
+        return entry.value;
+    }
+
+    public void remove(K key) {
+        Entry entry = getEntry(key);
+        if (entry != null) {
+            if (entry.pre != null) entry.pre.next = entry.next;
+            if (entry.next != null) entry.next.pre = entry.pre;
+            if (entry == first) first = entry.next;
+            if (entry == last) last = entry.pre;
+        }
+        hashMap.remove(key);
+    }
+
+    private void moveToFirst(Entry entry) {
+        if (entry == first) return;
+        if (entry.pre != null) entry.pre.next = entry.next;
+        if (entry.next != null) entry.next.pre = entry.pre;
+        if (entry == last) last = last.pre;
+
+        if (first == null || last == null) {
+            first = last = entry;
+            return;
+        }
+
+        entry.next = first;
+        first.pre = entry;
+        first = entry;
+        entry.pre = null;
+    }
+
+    private void removeLast() {
+        if (last != null) {
+            last = last.pre;
+            if (last == null) first = null;
+            else last.next = null;
+        }
+    }
+
+
+    private Entry<K, V> getEntry(K key) {
+        return hashMap.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Entry entry = first;
+        while (entry != null) {
+            sb.append(String.format("%s:%s ", entry.key, entry.value));
+            entry = entry.next;
+        }
+        return sb.toString();
+    }
+
+    class Entry<K, V> {
+        public Entry pre;
+        public Entry next;
+        public K key;
+        public V value;
+    }
+}
